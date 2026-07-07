280ad1a0b2a888d50f2ce17807f30e6c6b46cf1c
anapapait <ana.papait@escola.pr.gov.br>
2026-07-07 17:15:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,9 +5,38 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
   tamanhoSenha = tamanhoSenha-1;
-  numeroSenha.textContent = tamanhoSenha;.
-  
+  numeroSenha.textContent = tamanhoSenha;
+
+numeroSenha.textContent = tamanhoSenha;
+geraSenha();
 }
+function aumentaTamanho(){
+  if (tamanhoSenha < 20){
+    tamanhoSenha++;
+  }
+  numeroSenha.textContent = tamanhoSenha;
+  geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXWZ';
+geraSenha();
+
+campoSenha.value = letrasMaiusculas;
+
+function geraSenha(){
+  let senha = '';
+  for (let i=0; <tamanhoSenha;i++){
+  let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+  numeroAleatorio = Math.floor(numeroAleatorio);
+  senha = senha + letrasMaiusculas[numeroAleatorio];
+}
+campoSenha.value = senha;
+}
+
+
